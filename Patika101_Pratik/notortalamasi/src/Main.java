4b99f970cb31deb647c0e4d04287d8998dbe4465
rabiacemberci <rabiacemberci3@gmail.com>
2022-10-14 13:50:31 +0300

    Patika
    Patika için yaptığım çalışmalar.

---
+++ Patika101_Pratik/notortalamasi/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik notunuzu giriniz: ");
+        mat = input.nextInt();
+
+        System.out.print("Fizik notunuzu giriniz: ");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya notunuzu giriniz: ");
+        kimya = input.nextInt();
+
+        System.out.print("Türkçe notunuzu giriniz: ");
+        turkce = input.nextInt();
+
+        System.out.print("Tarih notunuzu giriniz: ");
+        tarih = input.nextInt();
+
+        System.out.print("Müzik notunuzu giriniz: ");
+        muzik = input.nextInt();
+
+        int sum = mat+fizik+kimya+turkce+tarih+muzik;
+        int ort = sum/6;
+        System.out.println("Not ortalamanız: " + ort);
+
+        String sonuc = ort > 60 ? "Sınıfı geçtiniz!" : "Sınıfta kaldınız!";
+        System.out.println(sonuc);
+    }
+}
