9afa85728d27101b07bd2d83dc3f5b20b3b61257
rabiacemberci <rabiacemberci3@gmail.com>
2022-10-18 21:34:43 +0300

    Patika

---
+++ Patika101_Odev/AsalSayiBulmaRecursive.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AsalSayiBulmaRecursive {
+    static boolean asalSayi(int a){
+        boolean isPrime = true;
+        for (int i=2;i<a;i++){
+            if (a%i==0) {
+                isPrime = false;
+                System.out.println(a + " sayısı ASAL değildir!");
+                break;
+            }else{
+                System.out.println(a + " sayısı ASALDIR!");
+                break;
+            }
+        }
+        return isPrime;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int a = input.nextInt();
+        System.out.println(asalSayi(a));
+
+    }
+
+}
